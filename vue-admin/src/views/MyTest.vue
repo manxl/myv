12e5af6af5a90f6b7788da8139f13d0b07523bbd
manxl <manxiaolei@sina.com>
2020-11-03 17:27:27 +0800

    add

---
+++ vue-admin/src/views/MyTest.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mymy">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <MyTestComponent msg="Welcome to Your Vue.js App 1111" />
+    <MyTestComponent2 msg="conponent 2" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MyTestComponent from "@/components/MyTestComponent.vue";
+
+export default {
+  name: "MyTest1",
+  components: {
+    MyTestComponent,
+    MyTestComponent2: () => import("@/components/MyTestComponent2.vue")
+  }
+};
+</script>
+
+<style>
+#mymy {
+  /* font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #30502c;
+  margin-top: 610px; */
+}
+</style>
